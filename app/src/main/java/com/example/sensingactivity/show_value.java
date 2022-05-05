234856fb5ade79657a5ef4b492eeb12bbf6716ae
Maria Jesus Dueñas <MJesus.duenas1@alu.uclm.es>
2022-05-05 03:17:50 +0200

    terminado sin codigo

--- app/src/main/java/com/example/sensingactivity/show_value.java
+++ app/src/main/java/com/example/sensingactivity/show_value.java
@@ -29,30 +29,21 @@ public class show_value extends Activity implements SensorEventListener {
     public String ip;
     Thread sensor;
     private TextView txt_valueX;
-
+    private TextView txt_valueY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_value);
 
-
-
-        Log.i("[Sensor Activity]", "Inicializating Sensor Services");
+        Log.i("[Sensor Activity]", "Inicializating sensor services");
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(show_value.this, acc, sensorManager.SENSOR_DELAY_NORMAL);
-        Log.i("[SENSOR ACTIVITY]", "Registered accelerometer listener.");
+        Log.i("[SENSOR ACTIVITY]", " Accelerometer registered.");
 
         txt_valueX= (TextView) findViewById(R.id.txt_valueX);
-        int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 11)
-        {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            new Thread (new ClientThread()).start();
-        }
+        txt_valueY= (TextView) findViewById(R.id.txt_valueY);
     }
 
     protected void onResume(){
@@ -60,22 +51,23 @@ public class show_value extends Activity implements SensorEventListener {
         sensorManager.registerListener(this,acc,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    public void onSensorchanged(SensorEvent sensorEvent){
-
-    }
-
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        float value_sensor= sensorEvent.values[0];
-        txt_valueX.setText(String.valueOf(value_sensor));
-        String info= "X: "+value_sensor;
+        float value_sensorX= sensorEvent.values[0];
+        float value_sensorY= sensorEvent.values[1];
+
+        txt_valueX.setText(String.valueOf(value_sensorX));
+        txt_valueY.setText(String.valueOf(value_sensorY));
+
+        String info= "X: "+value_sensorX + " "+ "Y" +value_sensorY;
         System.out.println(info);
+
         try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(
+            PrintWriter bf= new PrintWriter(new BufferedWriter(
                     new OutputStreamWriter(udp.getOutputStream())),
                     true);
-            out.println(info);
+            bf.println(info);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -86,36 +78,28 @@ public class show_value extends Activity implements SensorEventListener {
     }
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
+
     @Override
     protected void onStop() {
         sensorManager.unregisterListener(this, acc);
         super.onStop();
     }
-    public void onClickStart(View v){
+    public void button_Stop_click(View v){
         if (v.getId()==R.id.button_Stop) {
-            Log.i("[SENSOR ACTIVITY]", "Stopping Sensor Activity...\n");
+            Log.i("[SENSOR ACTIVITY]", "Stopping aplication...\n");
             onStop();
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
     }
-    public void button_Stop_click(View v){
-        sensorManager.unregisterListener(this, acc);
-        super.onStop();
-    }
-
     class ClientThread implements Runnable{
-
         @Override
         public void run() {
-            InetAddress server = null;
+            InetAddress s = null;
             try {
-
-                udp = new Socket(server, 8050);
-
+                udp = new Socket(s, 8050);
             }catch (UnknownHostException e) {
                 e.printStackTrace();
-
             }catch (IOException e) {
                 e.printStackTrace();
             }
